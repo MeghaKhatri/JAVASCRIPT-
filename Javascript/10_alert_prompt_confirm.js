372fcd1be6156d35388768e62d48f2a186d627cf
Megha khatri <119837712+MeghaKhatri@users.noreply.github.com>
2023-01-13 20:15:57 +0530

    Add files via upload

---
+++ Javascript/10_alert_prompt_confirm.js
@@ -0,0 +1,13 @@
+console.log("hii Megha")
+
+alert("hello alert show");
+
+var a = prompt("Enter yor name"); //take input from user
+
+var b = confirm("you want to print your name ?");
+if(b){
+    document.write(a);
+}
+else{
+    document.write("you denied to print your name");
+}
