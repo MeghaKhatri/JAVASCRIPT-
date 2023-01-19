148b3eadf2e970847d620e800cbbaaca54fc7452
Megha khatri <119837712+MeghaKhatri@users.noreply.github.com>
2023-01-19 20:25:38 +0530

    Add files via upload

---
+++ Javascript/15_setTimeOut.js
@@ -0,0 +1,29 @@
+// function myfun(name, time){
+//     document.write(`hii ${name} , good ${time}`);
+// }setTimeout(myfun, 8000,"megha", "morning");
+
+// by using arrow function
+// setTimeout(() => {
+//     alert("welcome to web page")
+// }, 6000);
+
+
+// pass refference in arrow function
+// setTimeout((a) => {
+//     alert (`hii ${a}`)
+// }, 3000, "megha");
+
+
+ //first function i.e. myfun will not be called
+// document.querySelector("button").addEventListener("click",()=>{  
+//     clearTimeout(1)
+// })
+
+
+
+// --------Time intervel-----------
+// time inetrval is repeat function after starting her time like you set 5 secend then function cal after every 5sec.
+
+// setInterval(() => {
+//     document.write("show in setInterval <br>")
+// }, 5000);
